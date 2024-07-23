93dcddf4cddc67b56f96cd86cb6ef940955e4fbf
Omar Santis <omarsantis@Laptop-de-Omar.local>
2024-07-23 17:46:27 -0500

    Le Medic

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				"verde-text-title-le-medic": "#385F5D"
+			},
+
+			backgroundImage: {
+				'hero-pattern': "url('/public/slide-le-medic-min.jpg')",
+				'footer-texture': "url('/img/footer-texture.png')",
+			}
+		},
+	},
+	plugins: [],
+}
